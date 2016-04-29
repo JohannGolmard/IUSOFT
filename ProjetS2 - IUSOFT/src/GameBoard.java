faa160e466aea97cb580c0a96ddaac3e2d26fe36
golmardj <golmardj@PC-A102-07.iut-valence.fr>
2016-04-29 15:26:08 +0200

    Creation of PlayerBoard and GameBoard class

--- ProjetS2 - IUSOFT/src/GameBoard.java
+++ ProjetS2 - IUSOFT/src/GameBoard.java
@@ -1,5 +1,32 @@
 
 public class GameBoard
 {
+	private PlayerBoard firstPlayerBoard;
+	private PlayerBoard secondPlayerBoard;
+	
+	public GameBoard(PlayerBoard fpb , PlayerBoard spb){
+		this.firstPlayerBoard=fpb;
+		this.secondPlayerBoard=spb;
+	}
 
+	public PlayerBoard getFirstPlayerBoard()
+	{
+		return firstPlayerBoard;
+	}
+
+	public void setFirstPlayerBoard(PlayerBoard firstPlayerBoard)
+	{
+		this.firstPlayerBoard = firstPlayerBoard;
+	}
+
+	public PlayerBoard getSecondPlayerBoard()
+	{
+		return secondPlayerBoard;
+	}
+
+	public void setSecondPlayerBoard(PlayerBoard secondPlayerBoard)
+	{
+		this.secondPlayerBoard = secondPlayerBoard;
+	}
+	
 }
---
+++ ProjetS2 - IUSOFT/src/PlayerBoard.java
@@ -0,0 +1,15 @@
+
+public class PlayerBoard
+{
+	private CardToPlay board;
+	//TODO adapter à la classe cardtoplay
+	
+	public PlayerBoard(CardToPlay board){
+		this.board=board;
+	}
+	public CardToPlay[] getBoard(){
+	//TODO adapter à la classe cardtoplay
+	//TODO write code
+	}
+	
+}
