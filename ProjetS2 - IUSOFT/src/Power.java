3d1411afe936435faf03e46c47e926e0172419cb
Adrien <Adrien@W7-Adrien>
2016-06-03 17:56:24 +0200

    New error corrections

--- ProjetS2 - IUSOFT/src/Power.java
+++ ProjetS2 - IUSOFT/src/Power.java
@@ -10,25 +10,5 @@ public class Power extends PlayableCard{
 	 */
 	public Power(int soulCost, int neededSanity, int neededResistance, int neededIntelligence, String trigger, String type) {
 		super(soulCost, neededSanity, neededResistance, neededIntelligence,trigger, type);
-		this.type=type;
-	}
-	/**
-	 * the type of the power
-	 */
-	private String type;
-
-	/**
-	 * returns the type of the power
-	 * @return type
-	 */
-	public String getType() {
-		return type;
-	}
-	/**
-	 * set the type of the power
-	 * @param type
-	 */
-	public void setType(String type) {
-		this.type = type;
 	}
 }
